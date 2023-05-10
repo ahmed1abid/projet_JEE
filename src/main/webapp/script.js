f71afaf0688f56e3b699a890b3c9a696cd1604fb
Laurent LIN <lol.laurentlin@gmail.com>
2023-05-10 16:07:49 +0200

    Auto stash before merge of "dev" and "origin/dev"

---
+++ src/main/webapp/script.js
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+async function loadStat(url){
+	const response = await fetch(url);
+	try {
+		var disciplines = await response.json();
+		process(disciplines)
+	} catch (error) {
+		alert("Erreur :" + error);
+	}
+	/*
+	fetch(url,{
+		body: JSON.stringify(requestBody)
+	})
+	.then(response => response.json())
+	.then(response => process(response))
+	.catch(error => alert("Erreur : " + error));
+	*/
+}
+
+function process(disciplines) {
+	var disciplineList = document.getElementById("disciplinelist");
+	
+	if (disciplineList.hasChildNodes()){
+		disciplineList.removeChild(disciplineList.firstChild);
+	}
+	
+	var table = document.createElement("table");
+	disciplineList.appendChild(table);
+	
+	console.log(disciplines);
+	for (var discipline of disciplines){
+		console.log(discipline);
+		var tr = document.createElement("tr");
+		var td_nom = document.createElement("td");
+		td_nom.append(discipline.name);
+		var td_nbSession = document.createElement("td");
+		td_nbSession.append("10");
+		
+		tr.appendChild(td_nom);
+		tr.appendChild(td_nbSession);
+		table.appendChild(tr);
+	}
+}
