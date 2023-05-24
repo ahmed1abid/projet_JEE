f27e04783107e6c27e1de63a2e043e40d56242b6
Axel_N'CHO <naxelamour09+github@gmail.com>
2023-05-24 18:19:54 +0200

    create & delete user

--- src/main/java/org/projetjee/users/UserDAO.java
+++ src/main/java/org/projetjee/users/UserDAO.java
@@ -1,9 +1,14 @@
 package org.projetjee.users;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public interface UserDAO {
 	
 	public ArrayList<User> findByUserName(String userName, String password);
+	
+	public boolean CreateUser(User user) throws NoSuchAlgorithmException;
+	
+	public boolean DeleteUser(String username);
 
 }
--- src/main/java/org/projetjee/users/UserDAOImpl.java
+++ src/main/java/org/projetjee/users/UserDAOImpl.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 import org.projetjee.DBManager;
 
@@ -26,6 +30,42 @@ public class UserDAOImpl implements UserDAO {
 			return users;
 		}
 		
+	}
+
+	@Override
+	public boolean CreateUser(User user) throws NoSuchAlgorithmException {
+		System.out.print(doHash(user.getPassword()));
+		return true;
+		/*
+		Connection conn = DBManager.getInstance().getConnection();
+		String query = String.format("select into user values(%s, %s, %s)", user.getUserName(),
+				this.doHash(user.getPassword()), user.getRole().toString());
+		try {
+			conn.createStatement().executeUpdate(query);
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}*/
+	}
+	
+	private String doHash(String message) throws NoSuchAlgorithmException {
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		byte[] encoded = digest.digest(message.getBytes(StandardCharsets.UTF_8));
+		return new String(Base64.getDecoder().decode(encoded));
+	}
+
+	@Override
+	public boolean DeleteUser(String username) {
+		Connection conn = DBManager.getInstance().getConnection();
+		String query = String.format("delete from user where username=%s", username);
+		try {
+			conn.createStatement().executeUpdate(query);
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 }
